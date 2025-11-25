6c381b3addce997f74573393584b5b707e819e3e
PetersOtto <52236754+PetersOtto@users.noreply.github.com>
2025-11-25 13:43:45 +0100

    Update remothemecss.php

--- remothemecss.php
+++ remothemecss.php
@@ -15,7 +15,7 @@ class YellowRemoThemeCss {
         $themeName = strtolower($this->yellow->system->get("theme"));
         $htmlSource = $text;
         preg_match('/<link.+href=[\'"]((?<=href=")[^"]+\.*'.preg_quote($themeName).'\.css)[\'"].*>/i', $text, $themeCssFileLink);
-        $output .= str_replace($themeCssFileLink[0],"<!-- The theme CSS link is not needed and was removed from the HTML source by the YellowRemoThemeCss extension. -->", $htmlSource);
+        $output .= str_replace($themeCssFileLink[0],"<!-- The theme CSS link is not needed and was removed from the HTML source code by the Yellow-RemoThemeCss extension. -->", $htmlSource);
         return $output;
     }
 }
